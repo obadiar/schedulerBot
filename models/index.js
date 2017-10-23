6f6c8183614c0b4090f5a4f04b653e4cabfa130e
Andros <wong22@hotmail.co.uk>
2017-10-23 14:58:21 -0700

    First commit to heroku

---
+++ models/index.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var connect = process.env.MONGODB_URI
+
+mongoose.connect(connect);
+
+var UserSchema = mongoose.Schema({
+  slackName: {
+    type: String
+  },
+  slackId: {
+    type: String
+  },
+  googleProfile: {
+    type: Object
+  }
+})
+
+var User = mongoose.model('User', UserSchema);
+
+
+module.exports = {
+  User: User
+}
