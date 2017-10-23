e53079366fe155acac5448d895c29209a5e31aec
Andros <wong22@hotmail.co.uk>
2017-10-23 15:00:03 -0700

    Remove node_modules and env.sh

--- env.sh
+++
@@ -1,9 +0,0 @@
-export GOOGLE_CLIENT_ID=636854958024-d5ad96g1kq1rg8igfmveg369ejvg2ck7.apps.googleusercontent.com
-
-export GOOGLE_CLIENT_SECRET=DT-gv_SeinY2RhxqjpFqumQv
-
-export MONGODB_URI=mongodb://andros:pass@ds129315.mlab.com:29315/maddy-scheduler
-
-export APIAI_TOKEN=c136184659b3408c9e4c912097b8f872
-
-export RTM_TOKEN=xoxb-261488258550-3d4HNgXomme0oXve6aUaSJua
